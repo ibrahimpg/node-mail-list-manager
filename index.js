c66345b25e59778d37ce975108769a01f5dbb4e9
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-22 01:47:23 -0900

    testing

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  db.collection('emails').findOne({ email: req.body.email })
+  db.find({ email: req.body.email })
     .then((emails) => res.json(emails))
     .catch((err) => res.status(500).json(err));
 });
