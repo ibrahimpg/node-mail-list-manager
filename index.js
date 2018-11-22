32f69b9e8243127fea6144f6cf087df41c0ed713
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-21 23:33:44 -0900

    implemented mongodb for initial testing

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const db;
+
+MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
+  if(err) throw err;
+  db = database;
+  app.listen(process.env.PORT || 8080);
+});
 
 app.use(express.json());
 app.use(cors());
 
 app.post('/', (req, res) => {
-  // code here
+  db.emails.insert( { item: req.body.email, qty: 15 } )
+    .then(() => res.json("Success!"))
+    .catch(() => res.status(500));
 })
-
-const port = process.env.PORT || 8080;
-
-app.listen(port);
