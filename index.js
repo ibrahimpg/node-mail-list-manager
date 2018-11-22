8fd69d164eea7c1d0f046ee0cb673a2cf846aa01
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-22 01:28:05 -0900

    testing get request

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/', (req, res) => {
-  db.emails.insertOne({email: req.body.email})
+  db.collection('emails').insertOne({email: req.body.email})
     .then(() => res.json("Success!"))
+    .catch((err) => res.status(500).json(err));
+});
+
+app.get('/', (req, res) => {
+  db.collection('emails').find({ email: req.body.email })
+    .then((emails) => res.json(emails))
     .catch((err) => res.status(500).json(err));
 });
