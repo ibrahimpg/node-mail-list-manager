f41c67925a01333756bec222d6c605179e870b21
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-22 01:33:18 -0900

    testing

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  db.collection('emails').find({ email: req.body.email })
+  db.collection('emails').find({ email: 'ibrahim.ghalioum@gmail.com' })
     .then((emails) => res.json(emails))
     .catch((err) => res.status(500).json(err));
 });
