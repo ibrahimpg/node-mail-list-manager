43e4f7cd443546156c830bf9ea27cd20fb4cbe09
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-21 23:54:17 -0900

    troubleshooting

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let db;
 MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
   if(err) throw err;
   db = database;
+  console.log(db);
   app.listen(process.env.PORT || 8080);
 });
 
@@ -16,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/', (req, res) => {
-  db.insert( { item: req.body.email } )
+  db.emails.insert( { item: req.body.email } )
     .then(() => res.json("Success!"))
     .catch(() => res.status(500));
 });
