70b35843d06ab0f43b4338ff1a9c7cb48e0fc710
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-22 02:25:22 -0900

    test

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
 app.use(express.json());
 app.use(cors());
 
-app.post('/', (req, res) => {
-  db.collection('emails').insertOne({email: req.body.email})
-    .then(() => res.json("Success!"))
-    .catch((err) => res.status(500).json(err));
-});
+// app.post('/', (req, res) => {
+//   db.collection('emails').insertOne({email: req.body.email})
+//     .then(() => res.json("Success!"))
+//     .catch((err) => res.status(500).json(err));
+// });
 
 app.get('/', (req, res) => {
   db.collection('emails').find({ email: req.body.email }).toArray()
