2f948dc8b48b80c539764789385246936e2fb786
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-23 00:52:48 -0900

    added return (bugfix)

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/unsubscribe/:email/:id', (req, res) => {
 
 app.post('/viewall', (req, res) => {
   if (req.body.password === process.env.PASSWORD) {
-    db.collection('subscribers').find().toArray()
+    return db.collection('subscribers').find().toArray()
       .then(subscribers => res.json(subscribers))
       .catch(() => res.sendStatus(500));
   }
