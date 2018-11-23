c4cf4c0b3205fb7e3f7e41891c40b89f17ab34b4
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-22 18:23:03 -0900

    testing new catch setup

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-underscore-dangle, no-unused-vars, no-bitwise */
 const express = require('express');
 const mongodb = require('mongodb');
 const cors = require('cors');
@@ -7,31 +7,28 @@ const app = express();
 
 let db;
 
-// mongodb.MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
-//   if (err) throw err;
-//   db = database.db();
-//   app.listen(process.env.PORT || 8080);
-// });
-
 mongodb.MongoClient.connect(process.env.MONGODB_URI)
   .then((database) => {
     db = database.db();
     app.listen(process.env.PORT || 8080);
   })
-  .catch(() => console.log('n'));
+  .catch((err) => {
+    throw err;
+  });
 
 app.use(express.json());
 app.use(cors());
 
 app.post('/', (req, res) => {
-  // eslint-disable-next-line no-unused-vars, no-bitwise
-  const _id = [...Array(10)].map(i => (~~(Math.random() * 36)).toString(36)).join('');
   db.collection('emails').find({ email: req.body.email }).toArray()
     .then((emails) => {
       if (emails.length > 0) {
         return res.json('email already exists yo!');
       }
-      return db.collection('emails').insertOne({ email: req.body.email, _id })
+      return db.collection('emails').insertOne({
+        email: req.body.email,
+        _id: [...Array(10)].map(i => (~~(Math.random() * 36)).toString(36)).join(''),
+      })
         .then(() => res.json('Success!'));
     })
     .catch(err => res.status(500).json(err));
