efbba6ba7608fbdc64c97becbb6d68cf967e2522
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-22 21:15:20 -0900

    implement email regex

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.post('/', (req, res) => {
   db.collection('subscribers').find({ email: req.body.email }).toArray()
     .then((subscribers) => {
-      if (subscribers.length === 0 && /^-?\d+\.?\d*$/.test(req.body.email) === true) {
+      if (subscribers.length === 0 && /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(req.body.email) === true) {
         return db.collection('subscribers').insertOne({
           email: req.body.email,
           _id: [...Array(10)].map(i => (~~(Math.random() * 36)).toString(36)).join(''),
