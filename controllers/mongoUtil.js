990243cacdebb9ab6c33a3197a7039c13316cf9d
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-23 14:00:03 -0900

    logging db...

--- controllers/mongoUtil.js
+++ controllers/mongoUtil.js
@@ -5,6 +5,7 @@ let _db;
 exports.connectToServer = (callback) => {
   mongodb.MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
     _db = database.db();
+    console.log(_db);
     return callback(err);
   });
 };
