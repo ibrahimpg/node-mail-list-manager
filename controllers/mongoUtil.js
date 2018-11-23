800eb10c1cc5cbd64c299ef959ad703262a866a0
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-23 13:49:02 -0900

    testing bugfix

--- controllers/mongoUtil.js
+++ controllers/mongoUtil.js
@@ -1,9 +1,9 @@
-import { MongoClient } from 'mongodb';
+const mongodb = require('mongodb');
 
 let _db;
 
 export function connectToServer(callback) {
-  MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
+  mongodb.MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
     _db = database.db();
     return callback(err);
   });
