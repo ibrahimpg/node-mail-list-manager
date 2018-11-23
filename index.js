3617d20fd075e5806604635786d52de0ffd0bad0
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-23 14:49:36 -0900

    test

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/unsubscribe/:email/:id', unsubscribe);
 
 app.post('/viewall', (req, res) => {
   if (req.body.password === process.env.PASSWORD) {
-    console.log(db);
+    console.log(db());
     return db().collection('subscribers').find().toArray()
       .then(subscribers => res.json(subscribers))
       .catch(() => res.sendStatus(500));
