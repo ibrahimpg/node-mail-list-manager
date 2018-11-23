6128a0af9cf5c069562ccfcd77ad6d343276f62f
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-23 13:54:15 -0900

    testing fixes

--- controllers/mongoUtil.js
+++ controllers/mongoUtil.js
@@ -2,12 +2,11 @@ const mongodb = require('mongodb');
 
 let _db;
 
-export function connectToServer(callback) {
+exports.connectToServer = (callback) => {
   mongodb.MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
     _db = database.db();
     return callback(err);
   });
-}
-export function getDb() {
-  return _db;
-}
+};
+
+exports.getDb = () => _db;
