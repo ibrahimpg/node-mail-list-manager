07ed80013a32b64285c1bfac6ea251070c77aefd
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-23 15:09:13 -0900

    test

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 // Packages
 const express = require('express');
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 const cors = require('cors');
 
 // MongoDB Connection Module
 const mongoUtil = require('./controllers/mongoUtil');
 
-const db = mongoUtil.getDb();
+const database = mongoUtil.getDb();
 
 // Controllers
 const subscribe = require('./controllers/subscribe');
@@ -44,30 +44,31 @@ app.get('/unsubscribe/:email/:id', unsubscribe);
 
 app.post('/viewall', (req, res) => {
   if (req.body.password === process.env.PASSWORD) {
-    console.log(db);
-    return db.collection('subscribers').find().toArray()
+    console.log(database);
+    console.log(database.Db);
+    return database.db().collection('subscribers').find().toArray()
       .then(subscribers => res.json(subscribers))
       .catch(() => res.sendStatus(500));
   }
   return res.sendStatus(400);
 });
 
-app.post('/', (req, res) => {
-  const transporter = nodemailer.createTransport({
-    service: 'Outlook365',
-    auth: { user: process.env.EMAIL_ADDRESS, pass: process.env.EMAIL_PASSWORD },
-  });
-  transporter.sendMail({
-    from: `"${process.env.ORG_NAME}" <${process.env.EMAIL_ADDRESS}>`,
-    to: req.body.email,
-    subject: req.body.subject,
-    text: `
-${req.body.message}
-// Unsubscribe link here. <URL>/user email/user id
-`,
-  })
-    .then(() => res.sendStatus(200))
-    .catch(() => res.sendStatus(500));
-});
+// app.post('/', (req, res) => {
+//   const transporter = nodemailer.createTransport({
+//     service: 'Outlook365',
+//     auth: { user: process.env.EMAIL_ADDRESS, pass: process.env.EMAIL_PASSWORD },
+//   });
+//   transporter.sendMail({
+//     from: `"${process.env.ORG_NAME}" <${process.env.EMAIL_ADDRESS}>`,
+//     to: req.body.email,
+//     subject: req.body.subject,
+//     text: `
+// ${req.body.message}
+// // Unsubscribe link here. <URL>/user email/user id
+// `,
+//   })
+//     .then(() => res.sendStatus(200))
+//     .catch(() => res.sendStatus(500));
+// });
 
 // Test route that sends the email to the env var email
