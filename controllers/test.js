15ee0c217b6a488681b77c0e54c67a1303de3e48
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-24 00:26:04 -0900

    testing different "from" setup in test route

--- controllers/test.js
+++ controllers/test.js
@@ -6,10 +6,10 @@ module.exports = (req, res) => {
     auth: { user: process.env.EMAIL_ADDRESS, pass: process.env.EMAIL_PASSWORD },
   });
   transporter.sendMail({
-    from: `"${process.env.ORG_NAME}" <${process.env.EMAIL_ADDRESS}>`,
+    from: process.env.EMAIL_ADDRESS,
     to: req.body.email,
     subject: req.body.subject,
-    html: `${req.body.html}`,
+    html: req.body.html,
   })
     .then(() => res.sendStatus(200))
     .catch(() => res.sendStatus(500));
