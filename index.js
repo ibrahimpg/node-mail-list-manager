2712e17ee2c375a642ae57923d142ff6993241f2
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-24 01:48:34 -0900

    projection fix

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/viewall', (req, res) => {
 app.post('/viewemails', (req, res) => {
   if (req.body.password === process.env.PASSWORD) {
     const db = mongoUtil.getDb();
-    return db.collection('subscribers').find({}, { _id: 0 }).toArray()
+    return db.collection('subscribers').find({}, { projection: { _id: 0 } }).toArray()
       .then(subscribers => res.json(subscribers))
       .catch(() => res.sendStatus(500));
   }
