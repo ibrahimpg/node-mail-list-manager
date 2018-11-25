b8151b52375fa6158bc0722c1207deed851f237f
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-25 01:18:02 -0900

    testing forEach

--- controllers/send.js
+++ controllers/send.js
@@ -6,12 +6,12 @@ module.exports = (req, res) => {
     const db = mongodb.getDb();
     return db.collection('subscribers').find({}, { projection: { _id: 0 } }).toArray()
       .then((subscribers) => { // has to be changed to a forEach or .map for unsub links to work
-        transporter.sendMail({
+        subscribers.forEach(transporter.sendMail({
           from: process.env.EMAIL_ADDRESS,
           to: subscribers,
           subject: req.body.subject,
-          html: `${req.body.html}<br><p>Unsubscribed by clicking the following link.</p>`,
-        })
+          html: `${req.body.html}<br><p>You are ${subscribers.email}</p>`,
+        }))
           .then(() => res.sendStatus(200));
       })
       .catch(() => res.sendStatus(500));
