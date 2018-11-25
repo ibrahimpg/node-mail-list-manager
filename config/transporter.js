45c52098ccf07f6ef835633eeb54b5c1fd40f6f7
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-25 01:58:46 -0900

    adding pool: true to transporter object

--- config/transporter.js
+++ config/transporter.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 
 module.exports = nodemailer.createTransport({
+  pool: true,
   service: 'Outlook365',
   auth: { user: process.env.EMAIL_ADDRESS, pass: process.env.EMAIL_PASSWORD },
 });
