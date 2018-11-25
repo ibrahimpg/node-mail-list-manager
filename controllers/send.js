ce76ecafdbf9dfe051179e3bc8763293868ce174
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-25 01:23:36 -0900

    testing catch inside nested promise

--- controllers/send.js
+++ controllers/send.js
@@ -8,11 +8,12 @@ module.exports = (req, res) => {
       .then((subscribers) => { // has to be changed to a forEach or .map for unsub links to work
         subscribers.forEach(transporter.sendMail({
           from: process.env.EMAIL_ADDRESS,
-          to: subscribers.email,
+          to: subscribers,
           subject: req.body.subject,
           html: `${req.body.html}<br><p>You are ${subscribers.email}</p>`,
         }))
-          .then(() => res.sendStatus(200));
+          .then(() => res.sendStatus(200))
+          .catch(err => console.log(err));
       })
       .catch(() => res.sendStatus(500));
   }
