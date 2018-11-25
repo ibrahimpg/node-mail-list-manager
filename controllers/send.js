427d4bfbcf49b6ef994effa1f118db2c23b57159
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-25 02:30:22 -0900

    show err for debugging

--- controllers/send.js
+++ controllers/send.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
   if (req.body.password === process.env.PASSWORD) {
     const db = mongodb.getDb();
     return db.collection('subscribers').find({}, { projection: { _id: 0 } }).toArray()
-      .then((subscribers) => { // has to be changed to a forEach or .map for unsub links to work
+      .then((subscribers) => {
         subscribers.forEach((subscriber) => {
           transporter.sendMail({
             from: process.env.EMAIL_ADDRESS,
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
           .then(() => res.sendStatus(200))
           .catch(err => res.json(err));
       })
-      .catch(() => res.sendStatus(500));
+      .catch(err => res.json(err));
   }
   return res.sendStatus(400);
 };
